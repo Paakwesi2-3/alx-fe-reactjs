bf610277155c5f22f128975791ec922a4bce579d
Paa Kwesi Larbi <paakwesilarbi2@gmail.com>
2025-09-12 10:11:22 +0000

    Add Counter component with increment, decrement, and reset

--- alx-react-app-new/src/App.js
+++ alx-react-app-new/src/App.js
@@ -1,14 +1,10 @@
-import React from "react";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div>
-      <Header />
-      <MainContent />
-      <Footer />
+      <h1>My React App</h1>
+      <Counter />
     </div>
   );
 }
